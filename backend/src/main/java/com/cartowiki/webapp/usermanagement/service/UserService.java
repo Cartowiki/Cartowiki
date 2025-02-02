d66a73a1e35fa2a6800184aee15bc088c333291a
Omykronn <omykronn.player@gmail.com>
2025-02-02 16:59:35 +0100

    Add user : check for size limit and encryption

--- backend/src/main/java/com/cartowiki/webapp/usermanagement/service/UserService.java
+++ backend/src/main/java/com/cartowiki/webapp/usermanagement/service/UserService.java
@@ -1,14 +1,24 @@
 package com.cartowiki.webapp.usermanagement.service;
 
+import java.util.Optional;
+
+import javax.naming.SizeLimitExceededException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.cartowiki.webapp.usermanagement.model.User;
+import com.cartowiki.webapp.usermanagement.repository.UserRepository;
 
 /**
  * Operations on User instances
  */
 @Service
 public class UserService {
+    @Autowired
+    UserRepository repository;
+
     /**
      * Check for the string fields' size according to the database restrictions :
      *  - mail : 128 char. max.
@@ -19,5 +29,26 @@ public class UserService {
      */
     public boolean checkFieldsSize(User user) {
         return user.getMail().length() <= 128 && user.getPseudo().length() <= 32 && user.getPassword().length() <= 128;
+    }
+
+    /**
+     * Save a new or existing user in the database
+     * @param user User to save
+     * @return Instance of the saved user
+     * @throws SizeLimitExceededException One field doesn't respect the database's limitations
+     */
+    public User addUser(User user) throws SizeLimitExceededException {
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        
+        user.setPassword(bCryptEncoder.encode(user.getPassword()));
+
+        if (this.checkFieldsSize(user)) {
+            user = repository.save(user);
+        }
+        else {
+            throw new SizeLimitExceededException("One field doesn't respect the database's limitations");
+        }
+
+        return user;
     }
 }
