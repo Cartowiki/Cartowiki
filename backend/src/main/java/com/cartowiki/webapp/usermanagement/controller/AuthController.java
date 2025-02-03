9d93eaaac61b8529e5eebbdf88bedd8a916b3a63
Omykronn <omykronn.player@gmail.com>
2025-02-03 14:56:23 +0100

    Add log in endpoint : generate token if valid credentials

--- backend/src/main/java/com/cartowiki/webapp/usermanagement/controller/AuthController.java
+++ backend/src/main/java/com/cartowiki/webapp/usermanagement/controller/AuthController.java
@@ -6,13 +6,18 @@ import javax.naming.SizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cartowiki.webapp.usermanagement.model.User;
+import com.cartowiki.webapp.usermanagement.model.request.LogInRequest;
 import com.cartowiki.webapp.usermanagement.model.request.SignUpRequest;
+import com.cartowiki.webapp.usermanagement.service.JwtService;
 import com.cartowiki.webapp.usermanagement.service.UserService;
 import com.cartowiki.webapp.util.ResponseMaker;
 
@@ -23,19 +28,26 @@ import com.cartowiki.webapp.util.ResponseMaker;
 @RequestMapping("/auth")
 public class AuthController {
     private UserService service;
+    private JwtService jwtService;
+    private AuthenticationManager authenticationManager;
 
     /**
      * Autowired controller
      * @param service Service for user management
+     * @param jwtService Service for JWT Authentication
+     * @param authenticationManager Authentication Manager
      */
     @Autowired
-    public AuthController(UserService service) {
+    public AuthController(UserService service, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.service = service;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     /**
      * Sign up endpoint
      * @param data User's data for sign up
+     * @return Response
      */
     @PostMapping("/signup")
     public ResponseEntity<Object> signUp(@RequestBody SignUpRequest data){
@@ -56,6 +68,27 @@ public class AuthController {
             catch (AuthenticationException e) {
                 response = ResponseMaker.singleValueResponse(ResponseMaker.ERROR, e.getMessage(), HttpStatus.CONFLICT);
             }
+        }
+
+        return response;
+    }
+
+    /**
+     * Log in endpoint : if successful, returns token
+     * @param data User's credentials
+     * @return Response
+     */
+    @PostMapping("/login")
+    public ResponseEntity<Object> authenticateAndGetToken(@RequestBody LogInRequest data) {
+        ResponseEntity<Object> response;
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(data.getUsername(), data.getPassword())
+        );
+
+        if (authentication.isAuthenticated()) {
+            response = ResponseMaker.singleValueResponse(ResponseMaker.TOKEN, jwtService.generateToken(data.getUsername()), HttpStatus.CREATED);
+        } else {
+            response = ResponseMaker.singleValueResponse(ResponseMaker.ERROR, "Invalid credentials", HttpStatus.UNAUTHORIZED);
         }
 
         return response;
