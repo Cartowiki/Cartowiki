34b461ec82298bdf19ee733cc4320325e1f76fbb
Omykronn <omykronn.player@gmail.com>
2025-02-03 10:53:06 +0100

    Add AuthController : sign up endpoint

---
+++ backend/src/main/java/com/cartowiki/webapp/usermanagement/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.cartowiki.webapp.usermanagement.controller;
+
+import javax.naming.SizeLimitExceededException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.cartowiki.webapp.usermanagement.model.User;
+import com.cartowiki.webapp.usermanagement.model.request.*;
+import com.cartowiki.webapp.usermanagement.service.UserService;
+
+/**
+ * Authentification requests manager
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    UserService service;
+
+    /**
+     * Sign up endpoint
+     * @param data User's data for sign up
+     * @return Response status code and message if error
+     */
+    @PostMapping("/signup")
+    public ResponseStatusException signUp(@RequestBody SignUpRequest data) {
+        if (data.getUsername().equals("") || data.getMail().equals("") || data.getPassword().equals("")) {
+            return new ResponseStatusException(HttpStatus.BAD_REQUEST, "Some arguments are missing");
+        }
+        else {
+            try {
+                // Create a new contributor
+                service.addUser(new User(data.getUsername(), data.getMail(), data.getPassword(), 0));
+                return new ResponseStatusException(HttpStatus.CREATED);
+            }
+            catch (SizeLimitExceededException e) {
+                return new ResponseStatusException(HttpStatus.BAD_REQUEST, "Some arguments are too long");
+            }
+        }
+    }
+}
