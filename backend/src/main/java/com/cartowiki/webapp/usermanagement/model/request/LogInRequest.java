2103ee982f538a88b03507bb4cd5e16073cdd60a
Omykronn <omykronn.player@gmail.com>
2025-02-03 12:49:56 +0100

    Add LogInRequest : data template for log in requests

---
+++ backend/src/main/java/com/cartowiki/webapp/usermanagement/model/request/LogInRequest.java
@@ -0,0 +1,49 @@
+package com.cartowiki.webapp.usermanagement.model.request;
+
+/**
+ * Data template for log in requests
+ */
+public class LogInRequest {
+    private String username;
+    private String password;
+    
+    /**
+     * No-argument constructor
+     */
+    public LogInRequest() {
+        this.username = "";
+        this.password = "";
+    }
+
+    /**
+     * Username getter
+     * @return Current username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Username setter
+     * @param username New username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Password getter
+     * @return Current password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Password setter
+     * @param password New password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
