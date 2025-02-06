87b6268c6aa1a5ec66dd036dedcb752467d919c7
Omykronn <omykronn.player@gmail.com>
2025-02-06 19:18:42 +0100

    Create login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
