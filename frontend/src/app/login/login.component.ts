2a65b7dcce9381ad69bb2b8d589bbdec7a59e7ef
Omykronn <omykronn.player@gmail.com>
2025-02-07 13:55:59 +0100

    Log in with backend server

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginData: FormGroup;
 
+  /**
+   * Constructor of login component
+   * @param _auth Authentication service for credential checks
+   * @param routers App routers
+   */
+  constructor( private _auth: AuthenticationService, private routers: Router) {
+    this.loginData = new FormGroup({username: new FormControl(), password: new FormControl()});
+  }
+
+  /**
+   * Loads login data on init
+   */
+  ngOnInit() {
+    this.loginData = new FormGroup({username: new FormControl(), password: new FormControl()});
+  }
+
+  /**
+   * Try logging in with the given credential
+   */
+  logIn(): void {
+    let username = this.loginData.value.username;
+    let password = this.loginData.value.password;
+
+    this._auth.checkLogin(username, password).subscribe({
+      next: data => {
+          console.log(data.token);
+      },
+      error: error => {
+          console.warn(error.error.message);
+      }
+    });
+  }
 }
