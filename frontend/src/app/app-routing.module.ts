c41b0ace0041e514efe4962cf0691df5900ffbca
Omykronn <omykronn.player@gmail.com>
2025-02-07 16:46:17 +0100

    Sign up component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 const routes : Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent}
 ]
 
 @NgModule({
