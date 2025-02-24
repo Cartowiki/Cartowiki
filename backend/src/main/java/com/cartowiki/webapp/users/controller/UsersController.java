14c261f9ad7d247722a231ce17dd0ce92c3f8964
Omykronn <omykronn.player@gmail.com>
2025-02-24 11:56:07 +0100

    Init UsersContoller

---
+++ backend/src/main/java/com/cartowiki/webapp/users/controller/UsersController.java
@@ -0,0 +1,13 @@
+package com.cartowiki.webapp.users.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Users manager
+ */
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+}
